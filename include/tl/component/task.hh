2299d1b42887a7d3fe655fae2656d59a8d897b7e
soul-coder-heap <1841531744@qq.com>
2021-07-29 18:30:52 +0800

    task && apply

--- include/tl/component/task.hh
+++ include/tl/component/task.hh
@@ -5,33 +5,34 @@
 
 namespace tl::blacklist {
 class task {
-public:
-    explicit task() {}
-    virtual ~task() noexcept {}
-    virtual void operator()() noexcept = 0;
+ public:
+  explicit task() {}
+  virtual ~task() noexcept {}
+  virtual void operator()() noexcept = 0;
 };
-
 
 template <typename Func, typename Result>
 class lambda_task final : public task {
-    Func _func;
-    Result _result;
-public:
-    lambda_task(Func&& func, Result&& result) : _func(std::move(func)), _result(std::move(result)) {}
-    virtual void operator()() noexcept override {
-		if constexpr(std::is_same_v<void, std::invoke_result_t<Func>>){
-        	_func();
-            _result();
-		}else{
-			auto r = _func();
-            _result(r);
-		}
+  Func _func;
+  Result _result;
+
+ public:
+  lambda_task(Func&& func, Result&& result)
+      : _func(std::move(func)), _result(std::move(result)) {}
+  virtual void operator()() noexcept override {
+    if constexpr (std::is_same_v<void, std::invoke_result_t<Func>>) {
+      _func();
+      _result();
+    } else {
+      auto r = _func();
+      _result(r);
     }
+  }
 };
 
 template <typename Func, typename Result>
-inline std::unique_ptr<task>
-make_task(Func&& func, Result&& result) {
-    return std::make_unique<lambda_task<Func, Result>>(std::forward<Func>(func), std::forward<Result>(result));
+inline std::unique_ptr<task> make_task(Func&& func, Result&& result) {
+  return std::make_unique<lambda_task<Func, Result>>(
+      std::forward<Func>(func), std::forward<Result>(result));
 }
-}// namespace tl::blacklist
+}  // namespace tl::blacklist
--- ut/task_test.cpp
+++ ut/task_test.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <type_traits>
 #include "tl/component/task.hh"
-
-template <typename Func>
-static inline auto apply(Func && func){
-    if(std::is_same_v<void, std::invoke_result_t<Func>>){
-        func();
-    }else{
-        return func();
-    }
-}
+#include "tl/component/apply.hh"
 
 int main(){
     using namespace tl::blacklist;
     apply([](){std::cout <<"haha"<< std::endl;});
     auto t = apply([](){return 1;});
+    apply([](int i, int j, double d){
+        std::cout <<"i = " <<i<< ", j = "<<j<<", d = "<<d<<std::endl;
+    },std::make_tuple(123,345564,3453.564));
     std::cout <<"t = "<<t<< std::endl;
     auto task = make_task([t]{
         std::cout <<"test"<< std::endl;
-        return t * 10001;
+        return apply([&t]{return t;});
     },[](auto &&val){
         std::cout <<"test finish : "<<val<< std::endl;
     });
