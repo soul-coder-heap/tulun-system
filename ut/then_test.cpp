7ac66a833a7ae4630ddad2a72f662ad9feaa58ec
soul-coder-heap <1841531744@qq.com>
2021-07-30 13:57:31 +0800

    lambad then

--- ut/then_test.cpp
+++ ut/then_test.cpp
@@ -1,16 +1,22 @@
 #include "tl/component/future.hh"
 #include <iostream>
+#include <string>
+void getVersion() {
+    std::string tag = "vr212123";
+    std::string date = "210730";
+    std::cout <<"start : "<<date<< std::endl;
+    std::cout <<"version : "<< tag << std::endl;
+}
 int main() {
   using namespace tl::blacklist;
-  future<int(int)> t([](int ii){std::cout <<"start"<< std::endl;return ii;});
-  t.then([](auto i){
-    std::cout <<"get i = "<<i<< std::endl;
+  future<void()> t(std::move(getVersion));
+  t.then([](){
+    std::cout <<"return 100 "<< std::endl;
     return 100;
   }).then([](auto j){
-        std::cout <<"finishi : "<<j<< std::endl;
-        //return 0;
+        std::cout <<"get laste return  : "<<j<< std::endl;
   }).then([](){
-      std::cout << "hahahhah" << std::endl;
+      std::cout << "hahahhaha" << std::endl;
       return 100;
   }).then([](auto i){
         std::cout <<"yyds : "<<i<< std::endl;
@@ -19,6 +25,6 @@ int main() {
      std::cout <<i<<" mobvista"<< std::endl;
   }).then([]{
     std::cout <<"byebye"<< std::endl;
-  }).then([]{}).run(3);
+  }).then([]{}).run();
   return 0;
 }
