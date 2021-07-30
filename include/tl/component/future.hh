c7856ec2b8da47e605cb6e78c1bb5b9a8b2961fc
yafeng.zhao <1841531744@qq.com>
2021-07-30 11:38:07 +0800

    future then debug

--- include/tl/component/future.hh
+++ include/tl/component/future.hh
@@ -1,54 +1,86 @@
 #include <future>
 #include <thread>
 #include <type_traits>
+#include "tl/component/function_traits.hh"
 
 namespace tl::blacklist {
+/*
 template <typename T>
-class future;
+std::enable_if_t<std::is_void_v<T>, T> simpleTypeInfo(T t) {
+  return t;
+}
 
-template <typename R, typename... Args>
-class future<R(Args...)> {
- public:
-  future(std::function<R(Args...)>&& f) : m_fn(std::move(f)) {}
-  future(const std::function<R(Args...)>& f) : m_fn(f) {}
+template <typename T>
+std::enable_if_t<not std::is_void_v<T>, T> simpleTypeInfo(T t) {
+  return t;
+}
+*/
+template <class R, class... Args>
+R getType(R(*)(Args...));
+
+template <typename T> class future;
+
+template <typename R, typename... Args> class future<R(Args...)> {
+public:
+  future(std::function<R(Args...)> &&f) : m_fn(std::move(f)) {}
+  future(const std::function<R(Args...)> &f) : m_fn(f) {}
   // R run(Args&&... args) { return m_fn(std::forward<Args>(args)...); }
-  R run(Args&&... args) {
-    return std::async(m_fn, std::forward<Args>(args)...);
-  }
-  template <typename... TArgs>
-  R get(TArgs&&... args) {
+  R run(Args &&... args) {
     if constexpr (std::is_void_v<R>) {
       std::async(m_fn, std::forward<Args>(args)...).get();
     } else {
       return std::async(m_fn, std::forward<Args>(args)...).get();
     }
   }
-  template <typename Func,
-            typename Ret = std::conditional_t<std::is_void_v<R>, 
-                                            std::result_of<Func(void)::type>,
-                                            std::result_of<Func(R)::type>>>
-  inline auto then(Func&& f) -> future<Ret> {
-    using return_type = std::invoke_result_t<Func>;
+  template <typename... TArgs> R get(TArgs &&... args) {
+    if constexpr (std::is_void_v<R>) {
+      std::async(m_fn, std::forward<TArgs>(args)...).get();
+    } else {
+      return std::async(m_fn, std::forward<TArgs>(args)...).get();
+    }
+  }
+  template <
+      typename Func /*, typename Ret = typename std::result_of<Func(R)>::type*/>
+  inline auto then(Func &&f)
+  /*-> future<
+  // typename std::result_of<Func(R)>::type(Args...)
+  std::conditional_t<std::is_void_v<int>,
+  typename std::result_of<Func(void)>::type(Args...),
+  typename std::result_of<Func(R)>::type(Args...)>> */
+  {
     auto func = std::move(m_fn);
-    return future<return_type(Args...)>([func, f](Args&&... args) {
-      auto lastf = std::async(func, std::forward<Args>(rags)...);
-      if constexpr (std::is_void_v<return_type>) {
-        if constexpr (std::is_void_v<R>) {
-          std::async(f).get;
+    // typedef typename std::result_of<Func(void)>::type VRet_; // void type
+    // error typedef typename std::result_of<Func(R)>::type Ret_; // return type
+    //using Ret = std::invoke_result_t<decltype(f)>;
+    if constexpr (std::is_void_v<R>) {
+      //using Ret = typename decltype(getType(f));
+      //using Ret = typename function_traits<decltype(Func{})>::return_type;
+      //using Ret = typename std::result_of<Func(R)>::type;
+      using Ret = typename std::result_of<Func(R)>::type;
+      //using Ret = typename std::invoke_result_t<decltype(f)>;
+      return future<Ret(Args...)>([func, f](Args &&... args) {
+        std::async(func, std::forward<Args>(args)...).get();
+        if constexpr (std::is_void_v<Ret>) {
+            std::async(f).get();
         } else {
-          std::async(f, lastf.get()).get();
+            return std::async(f).get;
         }
-      } else {
-        if constexpr (std::is_void_v<R>) {
-          return std::async(f).get;
+      });
+    } else {
+      using Ret = typename std::result_of<Func(R)>::type;
+      //using Ret = std::invoke_result_t<decltype(f)>;
+      return future<Ret(Args...)>([func, f](Args &&... args) {
+        auto lastf = std::async(func, std::forward<Args>(args)...);
+        if constexpr (std::is_void_v<Ret>) {
+            std::async(f, lastf.get()).get();
         } else {
-          return std::async(f, lastf.get()).get();
+            return std::async(f, lastf.get()).get();
         }
-      }
-    });
+      });
+    }
   }
 
- private:
+private:
   std::function<R(Args...)> m_fn;
 };
-}  // namespace tl::blacklist
+} // namespace tl::blacklist
--- ut/then_test.cpp
+++ ut/then_test.cpp
@@ -1,70 +1,20 @@
-#include <iostream>
 #include "tl/component/future.hh"
-void Testfuture(){
-    future<int(int)> task([](int i){std::cout <<"init :"<<i<< std::endl;return i;});
-    std::string str = "join in lambda then : ";
-    auto res = future.then([&str](int i){str += "then 1\t";std::cout <<"first:"<<i<<
-    	std::endl;return i + 1;})
-    .then([&str](int i){str += "then 2\t";std::cout <<"second:"<<i<<std::endl;return i + 2;})
-    .then([&str](int n){str += "then 3\t";std::cout <<"third:"<<n<<
-    	std::endl;return std::make_tuple(n + 3,"n + 3");})
-    .then([&str](auto n){str += "then 4\t";std::cout <<"fouth:"<<
-    	std::get<0>(n)<<"tuple string : "<<std::get<1>(n)<< std::endl;
-    	return std::make_tuple(std::get<0>(n) + 4,"then exec ecnd");
-    	}).run(1);
-    std::cout <<"result tuple value : "<<std::get<0>(res)<<"   result tuple string : "<<
-    	std::get<1>(res)<< "   result string : "<<str<<std::endl;
-}
-
-auto fu(int i){
-    auto f = std::async([](int i){
-        std::cout <<"sleep two "<<i<<" seconds..."<< std::endl;
-       // std::chrono::milliseconds dre(i * 1000);
-       // std::this_thread::sleep_for(dre);
-        return i * i;
-    },i);
-    return std::forward<decltype(f)>(f);
-}
-
-void testFuture(){
-    std::cout <<"at testFuture func thread : "<<std::this_thread::get_id()<< std::endl;
-    typedef std::future<int> A;
-    typedef std::result_of<decltype(fu)*(int)>::type B;
-    static_assert(std::is_same<A,B>::value,"not equal");
-    future<std::result_of<decltype(fu)&(int)>::type(int)> task(fu);
-    auto t = future.then([](auto o){
-        std::cout <<"wait value ..."<< std::endl;
-        std::cout <<"at first then func thread : "<<std::this_thread::get_id()<< std::endl;
-        auto ii = o.get() + 100;
-        auto f = std::async([](int i){
-            std::cout <<"new value :"<<i<<" .."<< std::endl;
-            std::cout <<"]async in thread : "<<std::this_thread::get_id()<< std::endl;
-            //std::chrono::milliseconds dre(i * 1000);
-            //std::this_thread::sleep_for(dre);
-            return i * i;
-        },ii);
-        return std::forward<decltype(f)>(f);})
-    .then([](auto o){
-        std::cout <<"wait value ...\n";
-        std::cout <<"at second then func thread : "<<std::this_thread::get_id<< std::endl;
-        //std::chrono::milliseconds dre(2000); //sleep 2 seconds
-        //std::this_thread::sleep_for(dre);
-        std::cout <<"wait last value...,,,,,,,,,,,,,,,,,,,,,,,,,,"<< std::endl;
-        return o.get() + 10000;})
-    .then([](auto o){
-        std::cout <<"fouth then"<< std::endl;
-        auto ff = std::async([](auto i){
-            std::cout <<"wait value ...\n";
-            std::cout <<"at third then func thread : "<<std::this_thread::get_id<< std::endl;
-            int all = 2134;
-            for(auto ii = 0; ii < i; ++ii){
-                //std::cout<<ii;
-                all += (ii % 3 == 0 && ii > all * 1.5) ? 1 : 2;
-            }
-            std::cout<<""<<all<<"\n";
-            return all;
-        },o);
-        return std::forward<decltype(ff)>(ff);})
-    .run(2); // 2 seconds
-    std::cout<<"future get : "<<t.get()<<"\n";
+#include <iostream>
+int main() {
+  using namespace tl::blacklist;
+  future<int(int)> t([](int ii){std::cout <<"start"<< std::endl;return ii;});
+  t.then([](auto i){
+    std::cout <<"get i = "<<i<< std::endl;
+    return 100;
+  }).then([](auto j){
+        std::cout <<"finishi : "<<j<< std::endl;
+        //return 0;
+  }).then([](){
+      std::cout << "hahahhah" << std::endl;
+      return 100;
+  }).then([](auto i){
+        std::cout <<"yyds : "<<i<< std::endl;
+        return i;
+  })//run(3);
+  return 0;
 }
